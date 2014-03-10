719301f83d76a59401f7b88623e81535150657e9
Rajas Shah <rajas.s.shah@gmail.com>
2014-03-10 11:25:20 +0530

    Mess Application and Library created.
    Signed-off-by: Rajas Shah <rajas.s.shah@gmail.com>

---
+++ Mess/Mess/login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mess;
+
+namespace Mess
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(";)");
+        }
+    }
+}
